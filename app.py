ecb66436d055ca8c05385b93a9c7c677efec390d
sushant3694 <43067972+sushant3694@users.noreply.github.com>
2026-07-19 19:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ if os.path.exists(MASTER_FILE):
     """, unsafe_allow_html=True)
 if df is not None and not df.empty:
     with st.form(key="search_form"):
-        search_query = st.text_input("Enter Your 8-Digit Application Number:", placeholder="e.g., 83362962").strip()
+        search_query = st.text_input("Enter Your 8-Digit Application Number:", placeholder="e.g., 12345678").strip()
         submit_button = st.form_submit_button(label="Verify Application Status")
 
     if submit_button and search_query:
